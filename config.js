af9e128404ed6b504c4eb12663d017053ec393d7
harsh dubey <74648792+Harshdube@users.noreply.github.com>
2022-06-30 16:07:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAkjMj8Ba58cLtzmlGPLmAibnaMn9yyIzU",
+  authDomain: "travelo-d18ae.firebaseapp.com",
+  databaseURL: "https://travelo-d18ae-default-rtdb.firebaseio.com",
+  projectId: "travelo-d18ae",
+  storageBucket: "travelo-d18ae.appspot.com",
+  messagingSenderId: "812306801947",
+  appId: "1:812306801947:web:7517b83d5eddf9c985dfbe"
+}
